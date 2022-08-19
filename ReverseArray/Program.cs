2359edb2a3847dc40a2d7af9280b3d02b822e2a4
Phillip Schmidt <pschmidt@truecoders.com>
2022-08-19 13:42:31 -0500

    initial commit

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,36 @@
+﻿var exampleArray = new int[10];
+var rng = new Random();
+for (var i = 0; i < exampleArray.Length; i++)
+{
+    exampleArray[i] = rng.Next(50);
+}
+
+Console.WriteLine("input array: ");
+foreach (var item in exampleArray)
+{
+    Console.WriteLine(item);
+}
+var reversedArray = Challenge.ReverseArray(exampleArray);
+
+Console.WriteLine("reversed array: ");
+foreach (var item in reversedArray)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadLine();
+
+class Challenge
+{
+    public static int[] ReverseArray(int[] input)
+    {
+        for (var i = 0; i < input.Length / 2; i++)
+        {
+            var temp = input[i];
+            input[i] = input[^(i + 1)];
+            input[^(i + 1)] = temp;
+        }
+
+        return input;
+    }
+}
