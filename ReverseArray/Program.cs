b0f9859ce7dcbbe99b0cb14bb72e2ffba782d1b9
Phillip Schmidt <pschmidt@truecoders.com>
2022-08-19 13:47:18 -0500

    minor

--- ReverseArray/Program.cs
+++ ReverseArray/Program.cs
@@ -1,17 +1,24 @@
 ﻿var exampleArray = new int[10];
+
+// populate input array with random numbers
 var rng = new Random();
 for (var i = 0; i < exampleArray.Length; i++)
 {
     exampleArray[i] = rng.Next(50);
 }
 
+// print original array to console
 Console.WriteLine("input array: ");
 foreach (var item in exampleArray)
 {
     Console.WriteLine(item);
 }
+
+//reverse it
 var reversedArray = Challenge.ReverseArray(exampleArray);
 
+
+// print results
 Console.WriteLine("reversed array: ");
 foreach (var item in reversedArray)
 {
